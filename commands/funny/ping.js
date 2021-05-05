56814331d2d546445e9ef6d49494cebc07be95da
RNCAT <xshootercast@gmail.com>
2021-05-05 19:19:30 +0700

    💬 add api ping response

--- commands/funny/ping.js
+++ commands/funny/ping.js
@@ -4,7 +4,12 @@ module.exports = {
   category: 'Funny',
   utilisation: '{prefix}ping',
   async execute(client, message) {
-    message.channel.send(`🌐 Ping : ${client.ws.ping}ms.`)
+    await message.channel.send('🌐 Pinging...').then((sent) => {
+      sent.edit(
+        `🌐 API Ping : ${sent.createdTimestamp - message.createdTimestamp}ms`
+      )
+    })
+    await message.channel.send(`🌐 Ping : ${client.ws.ping}ms.`)
     console.log(`${message.author.username} use command : ping`)
   },
 }

96a85d9e5a53f2a5f8339ebe625f9a62e15ec305
RNCAT <jaruwat.works@gmail.com>
2021-05-21 21:01:31 +0700

    :pencil2: change response message

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,31 +1,46 @@
-const fs = require("fs")
+const fs = require('fs')
 module.exports = {
-    name: 'play',
-    aliases: ['p'],
-    category: 'Music',
-    utilisation: '{prefix}play [name/URL]',
+  name: 'play',
+  aliases: ['p'],
+  category: 'Music',
+  utilisation: '{prefix}play [name/URL]',
 
-    async execute(client, message, args) {
-        if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`)
+  async execute(client, message, args) {
+    if (!message.member.voice.channel)
+      return message.channel.send(
+        `${client.emotes.error} - ยังไม่เข้า voice channel !`
+      )
 
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`${client.emotes.error} - You are not in the same voice channel !`)
+    if (
+      message.guild.me.voice.channel &&
+      message.member.voice.channel.id !== message.guild.me.voice.channel.id
+    )
+      return message.channel.send(
+        `${client.emotes.error} - เข้ามาห้องเดียวกันสิครับถ้าจะฟัง !`
+      )
 
-        if (!args[0]) return message.channel.send(`${client.emotes.error} - Please indicate the title of a song !`)
-        console.log(args.join(" "));
-        if (args.join(" ") === 'ปีศาจ') {
-            const connection = await message.member.voice.channel.join()
-            const dispatcher = connection.play(fs.createReadStream("./sound/cover.ogg"), { type: "ogg/opus" })
-            dispatcher.on("start", () => {
-              message.channel.send('พี่เบนซ์กำลังร้องเพลงปีศาจ')
-            })
-            dispatcher.on("finish", () => {
-                message.channel.send('พี่เบนซ์กำลังร้องเพลงจบแล้ว')
-              connection.disconnect()
-            })
-            dispatcher.on("error", console.error)
-        }else {
-            client.player.play(message, args.join(" "), { firstResult: true })
-        }
-        console.log(`${message.author.username} use command : play`)
-    },
+    if (!args[0])
+      return message.channel.send(
+        `${client.emotes.error} - ไม่บอกชื่อเพลงแล้วจะรู้ไหม !`
+      )
+
+    if (args.join(' ') === 'ปีศาจ') {
+      const connection = await message.member.voice.channel.join()
+      const dispatcher = connection.play(
+        fs.createReadStream('./sound/cover.ogg'),
+        { type: 'ogg/opus' }
+      )
+      dispatcher.on('start', () => {
+        message.channel.send('พี่เบนซ์กำลังร้องเพลงปีศาจ')
+      })
+      dispatcher.on('finish', () => {
+        message.channel.send('พี่เบนซ์กำลังร้องเพลงจบแล้ว')
+        connection.disconnect()
+      })
+      dispatcher.on('error', console.error)
+    } else {
+      client.player.play(message, args.join(' '), { firstResult: true })
+    }
+    console.log(`${message.author.username} use command : play`)
+  },
 }

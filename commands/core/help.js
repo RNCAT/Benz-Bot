58227350e626f0f1421217619d840588a114aa78
RNCAT <jaruwat.works@gmail.com>
2021-05-25 21:17:48 +0700

    :fire: remove unused files and func

--- commands/core/help.js
+++ commands/core/help.js
@@ -24,48 +24,6 @@ module.exports = {
           ],
         },
       })
-    } else {
-      const command =
-        message.client.commands.get(args.join(' ').toLowerCase()) ||
-        message.client.commands.find(
-          (x) => x.aliases && x.aliases.includes(args.join(' ').toLowerCase())
-        )
-
-      if (!command)
-        return message.channel.send(
-          `${client.emotes.error} - I did not find this command !`
-        )
-
-      message.channel.send({
-        embed: {
-          color: 'ORANGE',
-          author: { name: `วิธีใช้คำสั่ง ${command.name}` },
-          fields: [
-            { name: 'Name', value: command.name, inline: true },
-            {
-              name: 'Category',
-              value: command.category,
-              inline: true,
-            },
-            {
-              name: 'Aliase(s)',
-              value:
-                command.aliases.length < 1
-                  ? 'None'
-                  : command.aliases.join(', '),
-              inline: true,
-            },
-            {
-              name: 'Utilisation',
-              value: command.utilisation.replace(
-                '{prefix}',
-                client.config.discord.prefix
-              ),
-              inline: true,
-            },
-          ],
-        },
-      })
     }
     console.log(`${message.author.username} use command : help`)
   },

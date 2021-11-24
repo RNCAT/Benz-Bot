be1225df199105a5c16f026218b300c2949d5523
RNCAT <jaruwat.works@gmail.com>
2021-11-25 00:20:30 +0700

    :sparkles: allow command only in NSFW channel

--- commands/nsfw.ts
+++ commands/nsfw.ts
@@ -7,10 +7,19 @@ export default {
   description: 'Random a nsfw gif.',
   slash: true,
 
-  callback: async () => {
+  callback: async ({ channel }) => {
+    const embed = new MessageEmbed()
+
+    if (!channel.nsfw) {
+      embed.setColor('RED')
+      embed.setDescription('คำสั่งอนุญาตให้ใช้เฉพาะ channel NSFW เท่านั้น')
+
+      return embed
+    }
+
     const nsfw = await axios.get('https://nekobot.xyz/api/image?type=pgif')
 
-    const embed = new MessageEmbed().setImage(nsfw.data.message)
+    embed.setImage(nsfw.data.message)
 
     return embed
   },

bde8c61f8af2be7978725936b762ec6cd0d59e09
RNCAT <jaruwat.works@gmail.com>
2022-08-14 20:08:57 +0700

    fix: update config

--- src/config/environments.ts
+++ src/config/environments.ts
@@ -1,5 +1,7 @@
 const environments = {
   isProd: process.env.NODE_ENV === 'production',
+  bitlyToken: process.env.BITLY_TOKEN || '',
+  trnToken: process.env.TRN_TOKEN || '',
 }
 
 export default environments
